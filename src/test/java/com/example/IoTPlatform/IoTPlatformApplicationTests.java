31aee80a9fccb69bc213db29f803a4d4206cf8b2
Ramiiyan <Sriraguhan.2017380@iit.ac.lk>
2023-06-02 00:29:46 +0530

    initiated.

---
+++ src/test/java/com/example/IoTPlatform/IoTPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.IoTPlatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IoTPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
