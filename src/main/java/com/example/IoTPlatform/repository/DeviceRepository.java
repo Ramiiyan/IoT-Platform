39e8cb155d22ef0d3df521cb08b5a45c12dbb3d8
Ramiiyan <Sriraguhan.2017380@iit.ac.lk>
2023-08-12 15:14:35 +0530

    MQTT Service implemented

--- src/main/java/com/example/IoTPlatform/repository/DeviceRepository.java
+++ src/main/java/com/example/IoTPlatform/repository/DeviceRepository.java
@@ -9,7 +9,7 @@ import java.util.List;
 public interface DeviceRepository extends MongoRepository<Device, String> {
 
     @Query("{deviceName:'?0'}")
-    Device findItemByDeviceName(String deviceName);
+    List<Device> findItemByDeviceName(String deviceName);
 
     @Query(value = "{deviceType: '?0'}", fields="{'deviceId': 1,'deviceName':1, 'MqttPubPath':1,'MqttSubPath':1 }")
     List<Device> findAll(String deviceType);
