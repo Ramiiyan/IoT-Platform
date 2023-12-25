9080adb5f9c65e5c741402e9e8326a19882e2309
shrss-service-wso2-cloud <ServiceWSO2ManagedCloud@shrss.com>
2023-12-25 23:23:08 +0530

    Added the change

--- src/main/java/com/example/IoTPlatform/service/SocketService.java
+++ src/main/java/com/example/IoTPlatform/service/SocketService.java
@@ -2,10 +2,13 @@ package com.example.IoTPlatform.service;
 
 
 import com.corundumstudio.socketio.SocketIOClient;
+import com.example.IoTPlatform.model.Sensor;
 import com.example.IoTPlatform.model.SocketMsgDto;
 import com.example.IoTPlatform.model.SocketMessageType;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @Slf4j
