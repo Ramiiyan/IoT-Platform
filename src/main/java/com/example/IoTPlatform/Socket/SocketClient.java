33e68bb4ec9652efa0c5f90036840e740b573a69
Ramiiyan <sriraguhan.2017380@iit.ac.lk>
2024-08-20 09:27:57 +0530

    socketClient implementation changed.

--- src/main/java/com/example/IoTPlatform/Socket/SocketClient.java
+++ src/main/java/com/example/IoTPlatform/Socket/SocketClient.java
@@ -1,24 +1,15 @@
 package com.example.IoTPlatform.Socket;
 
-import com.example.IoTPlatform.model.Sensor;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import io.socket.client.IO;
 import io.socket.client.Socket;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
 
 import java.net.URISyntaxException;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.List;
 
 @Slf4j
+@Component
 public class SocketClient {
-
-    ObjectMapper objectMapper = new ObjectMapper();
-    final SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
     static private Socket socket;
     public Socket socketInit() {
         try {
@@ -35,24 +26,4 @@ public class SocketClient {
         socket.emit("send_message", payload);
     }
 
-    public void publishSocketDataToMongoDB(String msg){
-
-        List<Sensor> sensors = null;
-        Date currentDateTime;
-
-        try {
-            log.info("SensorData publisher to MongoDB");
-
-            currentDateTime = formatter.parse("<YYYY-mm-ddTHH:MM:ss>");
-            log.info("Current D&T: "+ currentDateTime.toString());
-            sensors = objectMapper.readValue(msg, new TypeReference<List<Sensor>>() {});
-            log.info("Sensor[0] Data:" + sensors.get(0).toString());
-//            for (Sensor sensor : sensors) {
-//                sensorDataService.saveSensorData(new SensorData(sensor.getSensorId(), currentDateTime, sensor.getsensorValue()));
-//            }
-            log.info("Data published to MongoDB");
-        } catch (JsonProcessingException | ParseException e){
-            throw new RuntimeException(e);
-        }
-    }
 }
